6d09f42174da48286571fb02bfb28b67545fa141
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-09 13:08:16 -0700

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    a Fabric script that distributes an archive to your web servers
+    using the function do_deploy
+"""
+from fabric.api import run, env, put
+
+env.hosts = ["52.201.220.63", "54.236.24.172"]
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """The fabric function for the process"""
+    if not archive_path:
+        return False
+    try:
+        archive_name = archive_path[9:len(archive_path) - 3]
+        put(archive_path, "/tmp/")
+        run(f"sudo mkdir -p /data/web_static/releases/{archive_name}")
+        run(f"sudo tar xzf /tmp/{archive_path[9:]} -C"
+            f"/data/web_static/releases/{archive_name}")
+        run(f"sudo rm -f /tmp/{archive_path[9:]}")
+        run("sudo rm -rf /data/web_static/current")
+        run(f"sudo mv /data/web_static/releases/{archive_name}/web_static/* "
+            f"/data/web_static/releases/{archive_name}")
+        run("sudo rm -rf /data/web_static/releases/{archive_name}/web_static")
+        run(f"sudo ln -sf /data/web_static/releases/{archive_name} "
+            "/data/web_static/current")
+        return True
+    except Exception:
+        return False
