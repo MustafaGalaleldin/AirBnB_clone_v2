072769f56768ca41d73088b812a9789579e4731c
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-12 03:05:02 +0300

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear(exception=None):
+    "tear down"
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    " display a HTML states page"
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
