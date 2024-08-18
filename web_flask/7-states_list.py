122c17a74aaadacd310d3ee75905512583865414
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-18 12:49:31 +0300

    check 7th

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,17 +7,17 @@ from models.city import City
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def tear(exception):
+    "tear down"
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     " display a HTML states page"
     states = sorted(list(storage.all(State).values()), key=lambda st: st.name)
     return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def tear(exception):
-    "tear down"
-    storage.close()
 
 
 if __name__ == '__main__':
