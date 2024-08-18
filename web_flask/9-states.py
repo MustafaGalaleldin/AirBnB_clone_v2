2145daf138b5fe3f9f34bee2ab8015bddd2ce578
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-18 10:31:27 +0300

    States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear(exception=None):
+    "tear down"
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    "show states"
+    states = sorted(list(storage.all(State).values()), key=lambda st: st.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_city(id):
+    "show cities with state_id"
+    existed = False
+    st_cities = None
+    states = storage.all(State).values()
+    for state in states:
+        if state.id == id:
+            existed = True
+            st_cities = state.cities
+            break
+    return render_template('9-states.html', existed=existed, cities=st_cities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
